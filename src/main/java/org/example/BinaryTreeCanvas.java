871912e681956e61030836f55f1916a432763aaa
non <tadchawan@geniustree.co.th>
2023-06-30 13:42:15 +0700

    edit BinarytreeCanvas

--- src/main/java/org/example/BinaryTreeCanvas.java
+++ src/main/java/org/example/BinaryTreeCanvas.java
@@ -24,36 +24,18 @@ public class BinaryTreeCanvas extends JLabel {
             if (root.key != null) {
                 g.setColor(Color.PINK);
                 g.fillOval(x - r, y - r, 2 * r, 2 * r);
-                g.setColor(Color.white);
+                g.setColor(Color.BLACK);
                 g.drawString(root.key.toString(), x - 5, y + 5);
             }
             if (root.left != null) {
-                drawLineLeft(g, x - xGap, y + yGap, x, y);
+                g.drawLine( x - xGap, y + yGap, x-r, y);
                 drawTree(g, root.left, x - xGap, y + yGap, xGap / 2);
             }
             if (root.right != null) {
-                drawLineRight(g, x + xGap, y + yGap, x, y);
+                g.drawLine(x + xGap, y + yGap, x+r, y);
                 drawTree(g, root.right, x + xGap, y + yGap, xGap / 2);
             }
         }
-    }
-
-    private void drawLineLeft(Graphics g, int x1, int y1, int x2, int y2) {
-        double d = Math.sqrt(yGap * yGap + (x2 - x1) * (x2 - x1));
-        int x11 = (int) (x1 + r * (x2 - x1) / d);
-        int x21 = (int) (x2 - r * (x2 - x1) / d);
-        int y11 = (int) (y1 - r * yGap / d);
-        int y21 = (int) (y2 + r * yGap / d);
-        g.drawLine(x11, y11, x21, y21);
-    }
-
-    private void drawLineRight(Graphics g, int x1, int y1, int x2, int y2) {
-        double d = Math.sqrt(yGap * yGap + (x2 - x1) * (x2 - x1));
-        int x11 = (int) (x1 - r * (x1 - x2) / d);
-        int x21 = (int) (x2 + r * (x1 - x2) / d);
-        int y11 = (int) (y1 - r * yGap / d);
-        int y21 = (int) (y2 + r * yGap / d);
-        g.drawLine(x11, y11, x21, y21);
     }
 
     public void insert(int key) {
